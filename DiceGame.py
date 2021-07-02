641a08141412f664dbbbe04f4e6a0cb17a88daea
s1280148 <s1280148@u-aizu.ac.jp>
2021-07-02 19:43:44 +0900

    Initial revision

---
+++ DiceGame.py
@@ -0,0 +1,11 @@
+import random
+
+print("Rolling the dice...")
+
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+
+print(f"Die1 : {dice1}")
+print(f"Die2 : {dice2}")
+
+print(f"Total value : {dice1 + dice2}")
